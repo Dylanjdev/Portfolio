106fc66e678f3b13d8f2a6780e3c24af2bb91c18
Dylan Smith <dylanjsmith.dev@gmail.com>
2025-10-26 03:46:01 -0400

    added vite again

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "https://github.com/Dylanjdev/Portfolio", // MUST match your GitHub repo name
+  base: "/https://github.com/Dylanjdev/Portfolio/", // MUST match your GitHub repo name
   plugins: [react()],
 });
